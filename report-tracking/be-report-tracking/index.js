4a47e71015900de3c60971abf7bc929914be54e5
Phan Văn Chiến - ITC - APP - Mobile App Developer <chien.phan@mservice.com.vn>
2022-10-25 15:47:21 +0700

    Add redirect when run automation test success

--- report-tracking/be-report-tracking/index.js
+++ report-tracking/be-report-tracking/index.js
@@ -3,14 +3,17 @@ const path = require("path");
 const fs = require("fs");
 const HTMLParser = require("node-html-parser");
 const { promisify } = require("util");
-const exec = require('child_process').exec;
+const exec = require("child_process").exec;
 
 const app = express();
 const port = 4000;
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
@@ -18,63 +21,76 @@ app.get("/", function (req, res) {
   res.send("Report API!");
 });
 
-app.get('/run', (req, res) => {
-    exec("cd /Users/ngan.dang/Testing/MiniAppCenter && mvn test --file /Users/ngan.dang/Testing/MiniAppCenter/pom.xml", function(error, stdout, stderr) {
-          if (!error) {
-                res.json({
-                     status: 'success'
-                })
-          } else {
-                res.json({
-                    status: 'failed',
-                    error: stdout,
-                })
-          };
-    });
+app.get("/run", (req, res) => {
+  exec(
+    "cd /Users/ngan.dang/Testing/MiniAppCenter && mvn test --file /Users/ngan.dang/Testing/MiniAppCenter/pom.xml",
+    function (error, stdout, stderr) {
+      if (!error) {
+        res.redirect("http://localhost:3000");
+      } else {
+        res.json({
+          status: "failed",
+          error: stdout,
+        });
+      }
+    }
+  );
 });
 
 app.get("/list-reports", function (req, res) {
+  const pageSize = Number(req?.query?.pageSize) || 10;
+  const pageIndex = Number(req?.query?.pageIndex) || 1;
 
-  const pageSize = Number(req?.query?.pageSize) || 10
-  const pageIndex = Number(req?.query?.pageIndex) || 1
-
-  const dirPath = path.join(__dirname, "../ExtentReports/ExtentReport.html");
+  const dirPath = path.join(__dirname, "../../ExtentReports/ExtentReport.html");
   const reportDirPath = path.join(__dirname, "/results");
   const getReportFiles = fs.readdirSync(reportDirPath);
 
-  const reportFiles = getReportFiles.reverse()
+  const reportFiles = getReportFiles.reverse();
 
-  const reportToGet = reportFiles.slice((pageIndex - 1) * pageSize, (pageSize * pageIndex))
+  const reportToGet = reportFiles.slice(
+    (pageIndex - 1) * pageSize,
+    pageSize * pageIndex
+  );
 
   const getData = () => {
     const fileList = [];
     const readFileAsync = promisify(fs.readFile);
-    Promise.all(
-      reportToGet.map((fileReport) => {
-        return readFileAsync(reportDirPath + `/${fileReport}`)
-      })
-    ).then((listReportFiles) => {
-      listReportFiles.forEach((file, index) => {
-        const root = HTMLParser.parse(file.toString());
-        const getStatus = root.querySelector(".badge.log.float-right");
-        const getName = root.querySelector(".test-detail .name");
-        const getTimeCheck = root.querySelector(".badge.badge-success");
-        fileList.push({
-          id: reportToGet[index].replace(".html", ""),
-          time: getTimeCheck.rawText,
-          description: getName.rawText,
-          status: getStatus.rawText,
-          key: reportToGet[index].replace(".html", ""),
+    try {
+      Promise.all(
+        reportToGet.map((fileReport) => {
+          return readFileAsync(reportDirPath + `/${fileReport}`);
+        })
+      ).then((listReportFiles) => {
+        listReportFiles.forEach((file, index) => {
+          const root = HTMLParser.parse(file.toString());
+          const getStatus = root.querySelector(".badge.log.float-right");
+          const getName = root.querySelector(".test-detail .name");
+          const getTimeCheck = root.querySelector(".badge.badge-success");
+          fileList.push({
+            id: reportToGet[index].replace(".html", ""),
+            time: getTimeCheck.rawText,
+            description: getName.rawText,
+            status: getStatus.rawText,
+            key: reportToGet[index].replace(".html", ""),
+          });
+        });
+        res.json({
+          total: getReportFiles.length,
+          pageSize: pageSize,
+          pageIndex: pageIndex,
+          data: fileList,
         });
       });
+    } catch (error) {
+      console.log(error);
       res.json({
-        total: getReportFiles.length,
-        pageSize: pageSize,
-        pageIndex: pageIndex,
-        data: fileList
+        total: 0,
+        pageSize: 1,
+        pageIndex: 1,
+        data: [],
       });
-    });
-  }
+    }
+  };
 
   function createUid(data) {
     const date = new Date(data);
@@ -89,7 +105,11 @@ app.get("/list-reports", function (req, res) {
       date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
 
     return (
-      `${year.toString().slice(-2)}` + `${month}` + `${day}` + `${hour}` + `${min}`
+      `${year.toString().slice(-2)}` +
+      `${month}` +
+      `${day}` +
+      `${hour}` +
+      `${min}`
     );
   }
 
@@ -122,7 +142,10 @@ app.get("/list-reports", function (req, res) {
 });
 
 app.get("/report/:fileName", function (req, res) {
-  const resultDirpath = path.join(__dirname, "/results/" + req.params.fileName + '.html');
+  const resultDirpath = path.join(
+    __dirname,
+    "/results/" + req.params.fileName + ".html"
+  );
   res.sendFile(resultDirpath);
 });
 
